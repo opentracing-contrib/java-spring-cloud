80e5fb0e3eeb3e33dffd03e8daee954423c634c0
Sergei Malafeev <sergeymalafeev@gmail.com>
2017-07-17 18:44:58 +0300

    don't import rxjava dependency from instrumentation lib #4

--- opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/rxjava/RxJavaTracingAutoConfiguration.java
+++ opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/rxjava/RxJavaTracingAutoConfiguration.java
@@ -4,10 +4,12 @@ package io.opentracing.contrib.spring.cloud.rxjava;
 import io.opentracing.Tracer;
 import io.opentracing.rxjava.TracingRxJavaUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@ConditionalOnClass(name = "rx.plugins.RxJavaHooks")
 public class RxJavaTracingAutoConfiguration {
 
   @Autowired
