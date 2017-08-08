c6eb7db689f445303711945c9974005d37790ce5
Pavol Loffay <ploffay@redhat.com>
2017-08-08 14:45:59 +0200

    Fix failed build

--- opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/feign/FeignManualTest.java
+++ opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/feign/FeignManualTest.java
@@ -2,14 +2,6 @@ package io.opentracing.contrib.spring.cloud.feign;
 
 import static io.opentracing.contrib.spring.cloud.feign.FeignTest.verify;
 
-import feign.Client;
-import feign.Feign;
-import feign.RequestLine;
-import io.opentracing.contrib.spring.cloud.MockTracingConfiguration;
-import io.opentracing.contrib.spring.cloud.TestController;
-import io.opentracing.contrib.spring.cloud.feign.FeignManualTest.ManualFeignConfiguration;
-import io.opentracing.contrib.spring.cloud.feign.FeignTest.FeignRibbonLocalConfiguration;
-import io.opentracing.mock.MockTracer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +10,17 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import feign.Client;
+import feign.Feign;
+import feign.RequestLine;
+import io.opentracing.contrib.spring.cloud.MockTracingConfiguration;
+import io.opentracing.contrib.spring.cloud.TestController;
+import io.opentracing.contrib.spring.cloud.feign.FeignManualTest.ManualFeignConfiguration;
+import io.opentracing.contrib.spring.cloud.feign.FeignTest.FeignRibbonLocalConfiguration;
+import io.opentracing.mock.MockTracer;
 
 /**
  * @author Pavol Loffay
@@ -41,6 +44,8 @@ public class FeignManualTest {
   @FeignClient(value = "localService")
   interface FeignInterface {
     @RequestLine("GET /notTraced")
+    // TODO this has to be added when using spring Boot 1.4.1
+    @RequestMapping(method = RequestMethod.GET, value = "/notTraced")
     String hello();
   }
 
