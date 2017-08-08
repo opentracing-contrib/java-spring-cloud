17475d4f5338d6a3ad2053995cd6ce1d92cf0717
Ales Justin <ales.justin@gmail.com>
2017-08-08 13:22:36 +0200

    Make ConnectionFactory lookup lazy.

--- opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/jms/JmsAutoConfiguration.java
+++ opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/jms/JmsAutoConfiguration.java
@@ -5,7 +5,9 @@ import javax.jms.Message;
 
 import io.opentracing.Tracer;
 import io.opentracing.contrib.jms.spring.TracingJmsTemplate;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.target.AbstractLazyCreationTargetSource;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,8 +29,22 @@ public class JmsAutoConfiguration {
 
   @Bean
   @ConditionalOnClass(JmsTemplate.class)
-  @ConditionalOnBean(ConnectionFactory.class)
-  public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory, Tracer tracer) {
+  public JmsTemplate jmsTemplate(BeanFactory beanFactory, Tracer tracer) {
+    ConnectionFactory connectionFactory = createProxy(beanFactory);
     return new TracingJmsTemplate(connectionFactory, tracer);
+  }
+
+  private ConnectionFactory createProxy(final BeanFactory beanFactory) {
+    return (ConnectionFactory) ProxyFactory.getProxy(new AbstractLazyCreationTargetSource() {
+      @Override
+      public synchronized Class<?> getTargetClass() {
+        return ConnectionFactory.class;
+      }
+
+      @Override
+      protected Object createObject() throws Exception {
+        return beanFactory.getBean(ConnectionFactory.class);
+      }
+    });
   }
 }
