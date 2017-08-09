5362384bd8e6f773c2a8d059d62ab974c3e6f81d
Ales Justin <ales.justin@gmail.com>
2017-08-09 15:50:09 +0200

    Fix scope, add msg wrt lazy usage.

--- opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/jms/JmsAutoConfiguration.java
+++ opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/jms/JmsAutoConfiguration.java
@@ -30,6 +30,9 @@ public class JmsAutoConfiguration {
   @Bean
   @ConditionalOnClass(JmsTemplate.class)
   public JmsTemplate jmsTemplate(BeanFactory beanFactory, Tracer tracer) {
+    // we create lazy proxy, to avoid dependency and config order
+    // if JMS is used, and Connectionfactory bean is not present,
+    // it will throw an error on first use, so imo, we should be all good
     ConnectionFactory connectionFactory = createProxy(beanFactory);
     return new TracingJmsTemplate(connectionFactory, tracer);
   }
