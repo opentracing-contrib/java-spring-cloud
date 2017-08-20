a3659f046aaa26b112ffa2849aa45c0cca74ca02
Eddú Meléndez <eddu.melendez@gmail.com>
2017-08-20 01:49:26 -0500

    Improve conditions in auto-configurations
    Move `ConditionalOnClass` to class level and add `ConditionalOnBean`
    Tracer to enable auto-configuration.

--- opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/jms/JmsAutoConfiguration.java
+++ opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/jms/JmsAutoConfiguration.java
@@ -8,30 +8,30 @@ import io.opentracing.contrib.jms.spring.TracingJmsTemplate;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.target.AbstractLazyCreationTargetSource;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author Eddú Meléndez
  */
 @Configuration
-@ConditionalOnClass(Message.class)
+@ConditionalOnClass({Message.class, JmsTemplate.class})
+@ConditionalOnBean(Tracer.class)
 public class JmsAutoConfiguration {
 
   @Bean
-  @ConditionalOnClass(JmsListener.class)
   public JmsListenerAspect jmsListenerAspect() {
     return new JmsListenerAspect();
   }
 
   @Bean
-  @ConditionalOnClass(JmsTemplate.class)
   public JmsTemplate jmsTemplate(BeanFactory beanFactory, Tracer tracer) {
     // we create lazy proxy, to avoid dependency and config order
-    // if JMS is used, and Connectionfactory bean is not present,
+    // if JMS is used, and ConnectionFactory bean is not present,
     // it will throw an error on first use, so imo, we should be all good
     ConnectionFactory connectionFactory = createProxy(beanFactory);
     return new TracingJmsTemplate(connectionFactory, tracer);
