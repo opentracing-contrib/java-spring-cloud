53d56c96a46542058faabc768b9717cf7733595a
Pavol Loffay <ploffay@redhat.com>
2017-09-08 15:05:00 +0200

    Enable custom async configurer

--- opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/async/CustomAsyncConfigurerAutoConfigurationTest.java
+++ opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/async/CustomAsyncConfigurerAutoConfigurationTest.java
@@ -11,18 +11,18 @@ import io.opentracing.contrib.spring.cloud.async.instrument.TracedAsyncConfigure
 /**
  * @author kameshs
  */
-public class AsyncCustomAutoConfigurationTest {
+public class CustomAsyncConfigurerAutoConfigurationTest {
 
     @Test
     public void should_return_bean_when_its_not_async_configurer() {
-        AsyncCustomAutoConfiguration configuration = new AsyncCustomAutoConfiguration();
+        CustomAsyncConfigurerAutoConfiguration configuration = new CustomAsyncConfigurerAutoConfiguration();
         Object bean = configuration.postProcessAfterInitialization(new Object(), "someBean");
         then(bean).isNotInstanceOf(TracedAsyncConfigurer.class);
     }
 
     @Test
     public void should_return_async_configurer_when_bean_instance_of_it() {
-        AsyncCustomAutoConfiguration configuration = new AsyncCustomAutoConfiguration();
+        CustomAsyncConfigurerAutoConfiguration configuration = new CustomAsyncConfigurerAutoConfiguration();
         Object bean = configuration.postProcessAfterInitialization(mock(AsyncConfigurer.class), "myAsync");
         then(bean).isInstanceOf(TracedAsyncConfigurer.class);
     }
