a89250804b8dc61227da13e5ed546ab68779b5f2
Pavol Loffay <p.loffay@gmail.com>
2017-09-20 16:50:00 +0200

    @Async, AsyncWebTask and Executor instrumentation (#43)
    * WIP - @Async implementation
    
    * WIP: Issue#35, fixing comments from @malafeev and @pavolloffay
    
    * Issue#35 - Added Unit and Integation Tests
    
    * Added io.opentracing.contrib.spring.cloud.async.AsyncCustomAutoConfiguration to spring.factories
    
    * Accidentally caused by previvous commit
    
    * WIP - Async Task - Test Cases
    
    * Resolving Issue#35 .,
    
    Handles instrumentation for
    
    * Async annotated methods
    * WebAsyncTask return types method for RestController/Controller
    * Callable return types method for RestController/Controller
    
    * Fix @Async, Executor and AsyncWebTash instrumentation
    
    * fix travis
    
    * enable custom configurer
    
    * completableFuture.join
    
    * add await to executor test
    
    * Enable custom async configurer
    
    * improve Async aspect
    
    * Add async to readme
    
    * Use traced Executor from contrib
    
    * Log exceptions for @Async
    
    * @Async inst, test tags
    
    * Fix licenses

---
+++ opentracing-spring-cloud/src/main/java/io/opentracing/contrib/spring/cloud/async/TracedAsyncWebAspect.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2017 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.opentracing.contrib.spring.cloud.async;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.Callable;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.web.context.request.async.WebAsyncTask;
+
+import io.opentracing.Tracer;
+import io.opentracing.contrib.concurrent.TracedCallable;
+
+/**
+ * this class adds tracing to all {@link org.springframework.stereotype.Controller}
+ * or {@link org.springframework.web.bind.annotation.RestController} that has
+ * <ul>
+ * <li>public {@link java.util.concurrent.Callable} methods</li>
+ * <li>public {@link org.springframework.web.context.request.async.WebAsyncTask} methods</li>
+ * </ul>
+ * All those methods which will eventually have {@link Callable#call()}
+ * will be wrapped with {@link io.opentracing.contrib.concurrent.TracedCallable}
+ * <p>
+ */
+@Aspect
+public class TracedAsyncWebAspect {
+
+    private Tracer tracer;
+
+    public TracedAsyncWebAspect(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+    private void anyRestControllerAnnotated() {
+    }
+
+    @Pointcut("@within(org.springframework.stereotype.Controller)")
+    private void anyControllerAnnotated() {
+    }
+
+    @Pointcut("execution(public java.util.concurrent.Callable *(..))")
+    private void anyPublicMethodReturningCallable() {
+    }
+
+    @Pointcut("(anyRestControllerAnnotated() || anyControllerAnnotated()) && anyPublicMethodReturningCallable()")
+    private void anyControllerOrRestControllerWithPublicAsyncMethod() {
+    }
+
+    @Pointcut("execution(public org.springframework.web.context.request.async.WebAsyncTask *(..))")
+    private void anyPublicMethodReturningWebAsyncTask() {
+    }
+
+    @Pointcut("(anyRestControllerAnnotated() || anyControllerAnnotated()) && anyPublicMethodReturningWebAsyncTask()")
+    private void anyControllerOrRestControllerWithPublicWebAsyncTaskMethod() {
+    }
+
+    @Around("anyControllerOrRestControllerWithPublicAsyncMethod()")
+    public Object tracePublicAsyncMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        final Callable<Object> delegate = (Callable<Object>) proceedingJoinPoint.proceed();
+        return new TracedCallable<>(delegate, tracer.activeSpan());
+    }
+
+    @Around("anyControllerOrRestControllerWithPublicWebAsyncTaskMethod()")
+    public Object tracePublicWebAsyncTaskMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        final WebAsyncTask<?> webAsyncTask = (WebAsyncTask<?>) proceedingJoinPoint.proceed();
+        Field callableField = WebAsyncTask.class.getDeclaredField("callable");
+        callableField.setAccessible(true);
+        // do not create span (there is always server span) just pass it to new thread.
+        callableField.set(webAsyncTask, new TracedCallable<>(webAsyncTask.getCallable(), tracer.activeSpan()));
+        return webAsyncTask;
+    }
+}
