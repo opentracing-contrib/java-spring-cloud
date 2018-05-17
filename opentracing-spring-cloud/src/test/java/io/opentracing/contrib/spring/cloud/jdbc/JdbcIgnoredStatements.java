f4a549ae1a0bd176d5954cf8d4854039e6c2473d
Will Penington <will@globaltravelventures.com>
2018-05-17 17:34:07 +0100

    Formatting fixes
    Fix indentation and licences headers on new test classes.

--- opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/jdbc/JdbcIgnoredStatements.java
+++ opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/jdbc/JdbcIgnoredStatements.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2017-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.opentracing.contrib.spring.cloud.jdbc;
 
 import static org.junit.Assert.assertEquals;
@@ -28,37 +42,37 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 })
 public class JdbcIgnoredStatements {
 
-    @Autowired
-    MockTracer tracer;
+  @Autowired
+  MockTracer tracer;
 
-    @Autowired
-    DataSource dataSource;
+  @Autowired
+  DataSource dataSource;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+  @Autowired
+  JdbcTemplate jdbcTemplate;
 
-    @Before
-    public void before() {
-        tracer.reset();
-    }
+  @Before
+  public void before() {
+    tracer.reset();
+  }
 
-    /**
-     * Make sure ignored statements aren't traced
-     */
-    @Test
-    public void spanIsNotCreatedForIgnoredStatement() throws SQLException {
-        PreparedStatement pstmt = dataSource.getConnection().prepareStatement("SELECT 1");
-        assertTrue(pstmt.execute());
-        assertEquals(0, tracer.finishedSpans().size());
-    }
+  /**
+   * Make sure ignored statements aren't traced
+   */
+  @Test
+  public void spanIsNotCreatedForIgnoredStatement() throws SQLException {
+    PreparedStatement pstmt = dataSource.getConnection().prepareStatement("SELECT 1");
+    assertTrue(pstmt.execute());
+    assertEquals(0, tracer.finishedSpans().size());
+  }
 
-    /**
-     * Make sure statements that aren't ignored aren't affected
-     */
-    @Test
-    public void spanIsCreatedNonIgnoredStatement() throws SQLException {
-        PreparedStatement pstmt = dataSource.getConnection().prepareStatement("SELECT 2");
-        assertTrue(pstmt.execute());
-        assertEquals(1, tracer.finishedSpans().size());
-    }
+  /**
+   * Make sure statements that aren't ignored aren't affected
+   */
+  @Test
+  public void spanIsCreatedNonIgnoredStatement() throws SQLException {
+    PreparedStatement pstmt = dataSource.getConnection().prepareStatement("SELECT 2");
+    assertTrue(pstmt.execute());
+    assertEquals(1, tracer.finishedSpans().size());
+  }
 }
