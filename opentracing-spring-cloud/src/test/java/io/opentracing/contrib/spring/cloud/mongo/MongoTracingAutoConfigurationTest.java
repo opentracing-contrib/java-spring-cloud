71e58abddd2360b91cfe84cb198cc58cc580f544
Vivien Maleze <vmaleze@gmail.com>
2018-05-23 10:49:39 +0200

    Fixed indentation and missing copyright header

--- opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/mongo/MongoTracingAutoConfigurationTest.java
+++ opentracing-spring-cloud/src/test/java/io/opentracing/contrib/spring/cloud/mongo/MongoTracingAutoConfigurationTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2017-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.opentracing.contrib.spring.cloud.mongo;
 
 import static org.hamcrest.Matchers.is;
@@ -14,35 +27,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @author  Vivien Maleze
+ * @author Vivien Maleze
  */
 public class MongoTracingAutoConfigurationTest {
 
-    @Test
-    public void loadMongoTracingByDefault() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(TracerConfig.class, MongoTracingAutoConfiguration.class);
-        context.refresh();
-        MongoClient mongoClient = context.getBean(MongoClient.class);
-        assertNotNull(mongoClient);
-    }
+  @Test
+  public void loadMongoTracingByDefault() {
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    context.register(TracerConfig.class, MongoTracingAutoConfiguration.class);
+    context.refresh();
+    MongoClient mongoClient = context.getBean(MongoClient.class);
+    assertNotNull(mongoClient);
+  }
 
-    @Test
-    public void disableMongoTracing() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(TracerConfig.class, MongoTracingAutoConfiguration.class);
-        EnvironmentTestUtils.addEnvironment(context, "opentracing.spring.cloud.mongo.enabled:false");
-        context.refresh();
-        String[] tracingMongoClientBeans = context.getBeanNamesForType(MongoClient.class);
-        assertThat(tracingMongoClientBeans.length, is(0));
-    }
+  @Test
+  public void disableMongoTracing() {
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    context.register(TracerConfig.class, MongoTracingAutoConfiguration.class);
+    EnvironmentTestUtils.addEnvironment(context, "opentracing.spring.cloud.mongo.enabled:false");
+    context.refresh();
+    String[] tracingMongoClientBeans = context.getBeanNamesForType(MongoClient.class);
+    assertThat(tracingMongoClientBeans.length, is(0));
+  }
 
-    @Configuration
-    static class TracerConfig {
+  @Configuration
+  static class TracerConfig {
 
-        @Bean
-        public Tracer tracer() {
-            return mock(Tracer.class);
-        }
+    @Bean
+    public Tracer tracer() {
+      return mock(Tracer.class);
     }
+  }
 }
