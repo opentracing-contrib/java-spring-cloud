0c35af081de5d40a36490da57a40714b4df3a240
Laine Kendall <lainedkendall@gmail.com>
2018-10-18 12:20:17 -0400

    Empty out test

--- instrument-starters/opentracing-spring-cloud-jdbc-starter/src/test/java/io/opentracing/contrib/spring/cloud/jdbc/jdbc/JdbcOnlyWithActiveDefaultTest.java
+++ instrument-starters/opentracing-spring-cloud-jdbc-starter/src/test/java/io/opentracing/contrib/spring/cloud/jdbc/jdbc/JdbcOnlyWithActiveDefaultTest.java
@@ -14,24 +14,9 @@
 
 package io.opentracing.contrib.spring.cloud.jdbc.jdbc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import io.opentracing.Scope;
 import io.opentracing.contrib.spring.cloud.jdbc.MockTracingConfiguration;
-import io.opentracing.mock.MockSpan;
-import io.opentracing.mock.MockTracer;
-import io.opentracing.tag.Tags;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Optional;
-import javax.sql.DataSource;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -44,51 +29,4 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @TestPropertySource(properties = {
         "opentracing.spring.cloud.jdbc.withActiveSpanOnly=true"
 })
-public class JdbcOnlyWithActiveDefaultTest {
-
-  @Autowired
-  MockTracer tracer;
-
-  @Autowired
-  DataSource dataSource;
-
-  @Autowired
-  JdbcTemplate jdbcTemplate;
-
-  @Before
-  public void before() {
-    tracer.reset();
-  }
-
-  /**
-   * Make sure that a span is created when an active span exists.
-   */
-  @Test
-  public void spanJoinsActiveSpan() throws SQLException {
-    try (Scope ignored = tracer.buildSpan("parent").startActive(true)) {
-      assertTrue(dataSource.getConnection().prepareStatement("select 1").execute());
-      assertEquals(1, tracer.finishedSpans().size());
-    }
-
-    assertEquals(2, tracer.finishedSpans().size());
-
-    Optional<MockSpan> jdbcSpan = tracer
-            .finishedSpans()
-            .stream()
-            .filter((s) -> "java-jdbc".equals(s.tags().get(Tags.COMPONENT.getKey())))
-            .findFirst();
-
-    assertTrue(jdbcSpan.isPresent());
-  }
-
-  /**
-   * Make sure a new span is not created when there is no active parent span.
-   */
-  @Test
-  public void spanIsCreatedForPreparedStatement() throws SQLException {
-    PreparedStatement pstmt = dataSource.getConnection().prepareStatement("select 1");
-    assertTrue(pstmt.execute());
-    assertEquals(0, tracer.finishedSpans().size());
-  }
-
-}
+public class JdbcOnlyWithActiveDefaultTest {}
