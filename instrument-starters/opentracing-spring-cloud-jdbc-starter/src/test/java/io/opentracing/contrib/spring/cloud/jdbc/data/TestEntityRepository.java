07597643edc993d3e6a872e8ece55f63d8827fad
Laine Kendall <laine_kendall@condenast.com>
2018-10-30 13:20:40 -0400

    Make JDBC tracing configurable through properties (#177)
    * Make JDBC tracer configurable through Spring properties
    
    Add properties for "withActiveSpanOnly" and "ignoreStatement" so
    the behaviour of the JDBC tracer can be controlled through
    application.yaml
    
    * Formatting fixes
    
    Fix indentation and licences headers on new test classes.
    
    * Fix pom ordering
    
    * Rename package, fix tests
    
    * Add default test, document properties
    
    * Revert "Add default test, document properties"
    
    This reverts commit bdbf55a065d44c5a99e6801c0327a683f1122369.
    
    * Add Default test
    
    * Add documentation for new properties
    
    * Revert "Add documentation for new properties"
    
    This reverts commit 5274ddb789a623795c5249b3d6361bb87a491893.
    
    * Revert "Add Default test"
    
    This reverts commit 1a72821b595ba21d0425bfdf9506a395c6f5c21a.
    
    * Revert "Revert "Add documentation for new properties""
    
    This reverts commit 47353f25bea84abf526b995ba0664d11becea208.
    
    * Add identical test
    
    * Empty out test
    
    * Add empty test
    
    * Delete defaults test

--- instrument-starters/opentracing-spring-cloud-jdbc-starter/src/test/java/io/opentracing/contrib/spring/cloud/jdbc/data/TestEntityRepository.java
+++ instrument-starters/opentracing-spring-cloud-jdbc-starter/src/test/java/io/opentracing/contrib/spring/cloud/jdbc/data/TestEntityRepository.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package io.opemtracing.contrib.spring.cloud.jdbc.data;
+package io.opentracing.contrib.spring.cloud.jdbc.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
