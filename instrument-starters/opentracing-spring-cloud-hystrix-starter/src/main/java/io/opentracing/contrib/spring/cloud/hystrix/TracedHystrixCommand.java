85938c902339ade1bbc9b70defed8a8b6882c2e6
wuyupengwoaini <1145048406@qq.com>
2019-02-19 16:31:34 +0800

    Annotate span with error when hystrix command fails (#199)
    * fix issue 198
    
    * checkstyle

--- instrument-starters/opentracing-spring-cloud-hystrix-starter/src/main/java/io/opentracing/contrib/spring/cloud/hystrix/TracedHystrixCommand.java
+++ instrument-starters/opentracing-spring-cloud-hystrix-starter/src/main/java/io/opentracing/contrib/spring/cloud/hystrix/TracedHystrixCommand.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-2018 The OpenTracing Authors
+ * Copyright 2017-2019 The OpenTracing Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -15,8 +15,12 @@ package io.opentracing.contrib.spring.cloud.hystrix;
 
 import com.netflix.hystrix.HystrixCommand;
 import io.opentracing.Scope;
+import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.tag.Tags;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Implement this class if you need traced variant of {@link HystrixCommand}. It creates a span that
@@ -39,15 +43,32 @@ public abstract class TracedHystrixCommand<R> extends HystrixCommand<R> {
   @Override
   protected R run() throws Exception {
     String commandKeyName = getCommandKey().name();
-
-    try (Scope scope = this.tracer.buildSpan(commandKeyName)
+    Scope scope = this.tracer.buildSpan(commandKeyName)
         .withTag(Tags.COMPONENT.getKey(), TAG_HYSTRIX_COMPONENT)
         .withTag(TAG_COMMAND_KEY, commandKeyName)
         .withTag(TAG_COMMAND_GROUP, commandGroup.name())
         .withTag(TAG_THREAD_POOL_KEY, threadPoolKey.name())
-        .startActive(true)) {
+        .startActive(true);
+    try {
       return doRun();
+    } catch (Exception e) {
+      onError(e,scope.span());
+      throw e;
+    } finally {
+      scope.close();
     }
+  }
+
+  private void onError(Exception e, Span span) {
+    Tags.ERROR.set(span, Boolean.TRUE);
+    span.log(errorLogs(e));
+  }
+
+  private Map<String, Object> errorLogs(Exception e) {
+    Map<String, Object> errorLogs = new HashMap<>(3);
+    errorLogs.put("event", Tags.ERROR.getKey());
+    errorLogs.put("error.object", e);
+    return errorLogs;
   }
 
   public abstract R doRun() throws Exception;
