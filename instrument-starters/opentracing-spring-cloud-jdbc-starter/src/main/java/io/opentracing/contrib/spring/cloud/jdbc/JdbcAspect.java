1a670fd1ec22e923e022c51b878ecd01791ca2ff
wuyupengwoaini <1145048406@qq.com>
2019-03-19 18:11:16 +0800

    fix ci test

--- instrument-starters/opentracing-spring-cloud-jdbc-starter/src/main/java/io/opentracing/contrib/spring/cloud/jdbc/JdbcAspect.java
+++ instrument-starters/opentracing-spring-cloud-jdbc-starter/src/main/java/io/opentracing/contrib/spring/cloud/jdbc/JdbcAspect.java
@@ -52,8 +52,8 @@ public class JdbcAspect {
   @Around("execution(java.sql.Connection *.getConnection(..)) && target(javax.sql.DataSource)")
   public Object getConnection(final ProceedingJoinPoint pjp) throws Throwable {
     Connection conn = (Connection) pjp.proceed();
-    if (conn instanceof TracingConnection) {
-        return conn;
+    if (conn instanceof TracingConnection) {      
+      return conn;
     }
     String url = conn.getMetaData().getURL();
     ConnectionInfo connectionInfo = URLParser.parser(url);
