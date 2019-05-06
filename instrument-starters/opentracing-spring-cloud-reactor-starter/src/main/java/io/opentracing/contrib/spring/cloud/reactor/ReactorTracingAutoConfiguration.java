ef3fcb64e522c1157ac6cbdda53400a3cc2cf1d7
Chengchen JI <cji@edgelab.ch>
2019-05-06 11:07:45 +0200

    Use a HookRegisteringPostProcessor to decorate Reactor as soon as possible (#216)
    * Use a HookRegisteringPostProcessor to decorate Reactor as soon as possible before potential early initialization of Reactor schedulers
    
    * log explicitly the exception to better debug the issue
    
    * use simply an ObjectFactory instead of an ObjectProvider to ease the error handling and improve the error message in the context of tracing
    
    * use  instead of  on ReactorTracingAutoConfiguration

--- instrument-starters/opentracing-spring-cloud-reactor-starter/src/main/java/io/opentracing/contrib/spring/cloud/reactor/ReactorTracingAutoConfiguration.java
+++ instrument-starters/opentracing-spring-cloud-reactor-starter/src/main/java/io/opentracing/contrib/spring/cloud/reactor/ReactorTracingAutoConfiguration.java
@@ -15,18 +15,28 @@
  */
 package io.opentracing.contrib.spring.cloud.reactor;
 
+import static java.util.logging.Level.SEVERE;
+
 import io.opentracing.Tracer;
 import io.opentracing.contrib.concurrent.TracedScheduledExecutorService;
 import io.opentracing.contrib.reactor.TracedSubscriber;
-import io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.logging.Logger;
 import javax.annotation.PreDestroy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Hooks;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 /**
@@ -37,27 +47,78 @@ import reactor.core.scheduler.Schedulers;
  * @author Csaba Kos
  */
 @Configuration
-@AutoConfigureAfter(TracerAutoConfiguration.class)
-@ConditionalOnBean(Tracer.class)
-@ConditionalOnClass(Hooks.class)
+@ConditionalOnClass({Tracer.class, Hooks.class})
 @ConditionalOnProperty(name = "opentracing.spring.cloud.reactor.enabled", havingValue = "true", matchIfMissing = true)
 public class ReactorTracingAutoConfiguration {
+
+  private static final Logger log = Logger.getLogger(ReactorTracingAutoConfiguration.class.getName());
+
   private static final String EXECUTOR_SERVICE_DECORATOR_KEY = ReactorTracingAutoConfiguration.class.getName();
   private static final String HOOK_KEY = ReactorTracingAutoConfiguration.class.getName();
 
-  @Autowired
-  public ReactorTracingAutoConfiguration(final Tracer tracer) {
-    Hooks.onEachOperator(HOOK_KEY, TracedSubscriber.asOperator(tracer));
-    Schedulers.setExecutorServiceDecorator(
-        EXECUTOR_SERVICE_DECORATOR_KEY,
-        (scheduler, scheduledExecutorService) ->
-            new TracedScheduledExecutorService(scheduledExecutorService, tracer)
-    );
+  @Bean
+  static HookRegisteringPostProcessor hookRegisteringPostProcessor(ObjectFactory<Tracer> tracerProvider) {
+    return new HookRegisteringPostProcessor(tracerProvider);
   }
 
-  @PreDestroy
-  public void cleanupHooks() {
-    Hooks.resetOnEachOperator(HOOK_KEY);
-    Schedulers.removeExecutorServiceDecorator(EXECUTOR_SERVICE_DECORATOR_KEY);
+  // Use a BeanDefinitionRegistryPostProcessor here to decorate Reactor as soon as possible before potential early initialization of Reactor schedulers
+  // For more detailed description, please refer to https://github.com/opentracing-contrib/java-spring-cloud/issues/214
+  private static class HookRegisteringPostProcessor implements BeanDefinitionRegistryPostProcessor {
+
+    private static final String ERROR_MSG = "Encountered error while retrieving Tracer instance! This reactive operation chain will not be instrumented.";
+
+    private final ObjectFactory<Tracer> tracerProvider;
+
+    private volatile Function<? super Publisher<Object>, ? extends Publisher<Object>> hookFunction;
+
+    private HookRegisteringPostProcessor(ObjectFactory<Tracer> tracerProvider) {
+      this.tracerProvider = tracerProvider;
+    }
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+      Hooks.onEachOperator(HOOK_KEY, hookFunctionWithFallback());
+      Schedulers.setExecutorServiceDecorator(EXECUTOR_SERVICE_DECORATOR_KEY, decoratorWithFallback());
+    }
+
+    private Function<? super Publisher<Object>, ? extends Publisher<Object>> hookFunctionWithFallback() {
+      return publisher -> {
+        try {
+          return getHookFunction().apply(publisher);
+        } catch (BeansException e) {
+          log.log(SEVERE, ERROR_MSG, e);
+          return publisher;
+        }
+      };
+    }
+
+    private Function<? super Publisher<Object>, ? extends Publisher<Object>> getHookFunction() {
+      return hookFunction == null ?
+             hookFunction = TracedSubscriber.asOperator(tracerProvider.getObject()) :
+             hookFunction;
+    }
+
+    private BiFunction<Scheduler, ScheduledExecutorService, ScheduledExecutorService> decoratorWithFallback() {
+      return (scheduler, scheduledExecutorService) -> {
+        try {
+          return new TracedScheduledExecutorService(scheduledExecutorService, tracerProvider.getObject());
+        } catch (BeansException e) {
+          log.log(SEVERE, ERROR_MSG, e);
+          return scheduledExecutorService;
+        }
+      };
+    }
+
+    @PreDestroy
+    public void cleanupHooks() {
+      Hooks.resetOnEachOperator(HOOK_KEY);
+      Schedulers.removeExecutorServiceDecorator(EXECUTOR_SERVICE_DECORATOR_KEY);
+    }
+
   }
+
 }
