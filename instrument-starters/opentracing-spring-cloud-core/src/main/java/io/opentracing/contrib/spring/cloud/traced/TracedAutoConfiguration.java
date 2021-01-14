76fc7de41a7c26590c9fff59d7ef211d87f7cc3f
Louis-Etienne <ledor473@hotmail.com>
2021-01-14 02:08:03 -0500

    Add @Traced plus MethodInterceptorSpanDecorator with a standard implementation and move shared logic from @Scheduled/@Traced to a base class (#302)

--- instrument-starters/opentracing-spring-cloud-core/src/main/java/io/opentracing/contrib/spring/cloud/traced/TracedAutoConfiguration.java
+++ instrument-starters/opentracing-spring-cloud-core/src/main/java/io/opentracing/contrib/spring/cloud/traced/TracedAutoConfiguration.java
@@ -11,52 +11,57 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package io.opentracing.contrib.spring.cloud.scheduled;
+package io.opentracing.contrib.spring.cloud.traced;
 
 import io.opentracing.Tracer;
-
 import io.opentracing.contrib.spring.cloud.aop.MethodInterceptorSpanDecorator;
 import io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration;
 import java.util.ArrayList;
 import java.util.List;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.util.CollectionUtils;
 
-/**
- * @author Pavol Loffay
- */
 @Configuration
 @ConditionalOnBean(Tracer.class)
 @AutoConfigureAfter(TracerAutoConfiguration.class)
-@ConditionalOnProperty(name = "opentracing.spring.cloud.scheduled.enabled", havingValue = "true", matchIfMissing = true)
-@EnableConfigurationProperties(ScheduledTracingProperties.class)
-public class ScheduledAutoConfiguration {
+@ConditionalOnProperty(name = "opentracing.spring.cloud.traced.enabled", havingValue = "true", matchIfMissing = true)
+@ConditionalOnWebApplication
+@ConditionalOnClass({ProceedingJoinPoint.class})
+@EnableConfigurationProperties(TracedTracingProperties.class)
+public class TracedAutoConfiguration {
 
   private final ObjectProvider<List<MethodInterceptorSpanDecorator>> methodInterceptorSpanDecorators;
 
-  public ScheduledAutoConfiguration(ObjectProvider<List<MethodInterceptorSpanDecorator>> methodInterceptorSpanDecorators) {
+  public TracedAutoConfiguration(ObjectProvider<List<MethodInterceptorSpanDecorator>> methodInterceptorSpanDecorators) {
     this.methodInterceptorSpanDecorators = methodInterceptorSpanDecorators;
   }
 
   @Bean
-  public ScheduledAspect scheduledAspect(Tracer tracer, ScheduledTracingProperties scheduledTracingProperties) {
+  @ConditionalOnMissingBean
+  public TracedAspect tracedAspect(Tracer tracer, TracedTracingProperties tracedTracingProperties) {
     List<MethodInterceptorSpanDecorator> spanDecorators = new ArrayList<>();
     spanDecorators.add(new MethodInterceptorSpanDecorator.StandardTags());
 
-    List<MethodInterceptorSpanDecorator> providedDecorators = this.methodInterceptorSpanDecorators.getIfAvailable();
-    if (!CollectionUtils.isEmpty(providedDecorators)) {
-      providedDecorators = new ArrayList<>(providedDecorators);
-      AnnotationAwareOrderComparator.sort(providedDecorators);
-      spanDecorators.addAll(providedDecorators);
+    List<MethodInterceptorSpanDecorator> decorators = this.methodInterceptorSpanDecorators
+        .getIfAvailable();
+    if (!CollectionUtils.isEmpty(decorators)) {
+      decorators = new ArrayList<>(decorators);
+      AnnotationAwareOrderComparator.sort(decorators);
+      spanDecorators.addAll(decorators);
     }
 
-    return new ScheduledAspect(tracer, scheduledTracingProperties, spanDecorators);
+    return new TracedAspect(tracer, tracedTracingProperties, spanDecorators);
   }
+
 }
---
+++ instrument-starters/opentracing-spring-cloud-core/src/test/java/io/opentracing/contrib/spring/cloud/traced/TracedAspectTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2017-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.opentracing.contrib.spring.cloud.traced;
+
+import static org.mockito.Mockito.when;
+
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.spring.cloud.aop.MethodInterceptorSpanDecorator;
+import io.opentracing.contrib.spring.cloud.aop.Traced;
+import java.util.List;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.assertj.core.api.WithAssertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TracedAspectTest implements WithAssertions {
+
+  @Mock
+  private Span span;
+  @Mock
+  private Tracer tracer;
+  @Mock
+  private List<MethodInterceptorSpanDecorator> decorators;
+  @Mock
+  private ProceedingJoinPoint pjp;
+  @Mock
+  private MethodSignature signature;
+  @Mock
+  private TracedTracingProperties tracedTracingProperties;
+
+  private TracedAspect aspect;
+
+  @Before
+  public void init() {
+    when(pjp.getSignature()).thenReturn(signature);
+    when(tracedTracingProperties.getSkipPattern()).thenReturn("");
+    aspect = new TracedAspect(tracer, tracedTracingProperties, decorators);
+  }
+
+  @Test
+  public void givenCustomOperation_whenGettingOperationName_thenOperationNameShouldBeOverriden()
+      throws Throwable {
+    when(signature.getMethod()).thenReturn(TracedClass.class.getMethod("tracedOperation"));
+
+    assertThat(aspect.getOperationName(pjp)).isEqualTo("customOps");
+  }
+
+  @Test
+  public void givenEmptyOperation_whenGettingOperationName_thenOperationNameShouldBeMethodName()
+      throws Throwable {
+    when(signature.getMethod()).thenReturn(TracedClass.class.getMethod("traced"));
+
+    assertThat(aspect.getOperationName(pjp)).isEqualTo("traced");
+  }
+
+
+  @Test
+  public void givenCustomComponent_whenGettingComponent_thenComponentNameShouldBeOverriden()
+      throws Throwable {
+    when(signature.getMethod()).thenReturn(TracedClass.class.getMethod("tracedComponent"));
+
+    assertThat(aspect.getComponent(pjp)).isEqualTo("custom");
+  }
+
+  @Test
+  public void givenDefaultComponent_whenGettingComponent_thenComponentNameShouldBeOverriden()
+      throws Throwable {
+    when(signature.getMethod()).thenReturn(TracedClass.class.getMethod("traced"));
+
+    assertThat(aspect.getComponent(pjp)).isEqualTo("traced");
+  }
+
+  public static class TracedClass {
+
+    @Traced
+    public void traced() {
+
+    }
+
+    @Traced(operationName = "customOps")
+    public void tracedOperation() {
+
+    }
+
+    @Traced(component = "custom")
+    public void tracedComponent() {
+
+    }
+  }
+
+}
